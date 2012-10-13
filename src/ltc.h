b319a6bd6ef10932fb77a207ee28b9f4123322dd
Robin Gareus <robin@gareus.org>
2012-10-13 02:03:19 +0200

    finalize changelog v0.6.0

--- src/ltc.h
+++ src/ltc.h
@@ -34,17 +34,17 @@ extern "C" {
 
 #ifndef DOXYGEN_IGNORE
 /* libltc version */
-#define LIBLTC_VERSION "0.5.3"
+#define LIBLTC_VERSION "0.6.0"
 #define LIBLTC_VERSION_MAJOR  0
-#define LIBLTC_VERSION_MINOR  5
-#define LIBLTC_VERSION_MICRO  3
+#define LIBLTC_VERSION_MINOR  6
+#define LIBLTC_VERSION_MICRO  0
 
 /* interface revision number
  * http://www.gnu.org/software/libtool/manual/html_node/Updating-version-info.html
  */
-#define LIBLTC_CUR  4
+#define LIBLTC_CUR  5
 #define LIBLTC_REV  0
-#define LIBLTC_AGE  2
+#define LIBLTC_AGE  3
 #endif
 
 /**
