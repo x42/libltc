5d3e1a0d8aa39d8cfd0c21922cbe8834132f5379
Robin Gareus <robin@gareus.org>
2012-11-02 01:20:07 +0100

    accomodate OSX

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-aclocal && autoheader && libtoolize --copy -f && autoconf && automake --gnu --add-missing --copy -f && ./configure $@
+if test -z "$LIBTOOLIZE"; then
+  if test "$(uname)" == "Darwin"; then
+    LIBTOOLIZE="glibtoolize"
+  else
+    LIBTOOLIZE="libtoolize"
+  fi
+fi
+
+aclocal && autoheader && $LIBTOOLIZE --copy -f && autoconf && automake --gnu --add-missing --copy -f && ./configure $@
