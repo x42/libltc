a99e6e1e0efea73c993c1873149a73491f2031b9
Robin Gareus <robin@gareus.org>
2012-11-02 01:26:52 +0100

    update LGPL boilerplate

--- src/decoder.c
+++ src/decoder.c
@@ -9,19 +9,18 @@
    by Tom Torfs - donated to the public domain
 
    This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU Lesser Public License as published by
-   the Free Software Foundation; either version 3, or (at your option)
-   any later version.
+   it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Lesser Public License for more details.
+   GNU Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
+   License along with this library.
+   If not, see <http://www.gnu.org/licenses/>.
 */
 
 /** turn a numeric literal into a hex constant
