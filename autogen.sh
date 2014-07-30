a6e6f07a0a8c0b7c59151692f2a3cf19e8438f19
Jonas Petersen <jnsptrsn1@gmail.com>
2014-07-30 21:31:09 +0200

    Fixed 'equals' operator. In shell scripts it's '=', not '=='.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if test -z "$LIBTOOLIZE"; then
-  if test "$(uname)" == "Darwin"; then
+  if test "$(uname)" = "Darwin"; then
     LIBTOOLIZE="glibtoolize"
   else
     LIBTOOLIZE="libtoolize"
